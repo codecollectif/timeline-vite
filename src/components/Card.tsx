9b329117a28fd16be702c647efe3807c5cc6bc3a
elodie0530 <uneondine@gmail.com>
2025-11-25 12:51:26 +0100

    create card

---
+++ src/components/Card.tsx
@@ -0,0 +1,15 @@
+function Card() {
+  return (
+    <figure>
+      <img src="https://picsum.photos/200" alt="Lorem ipsum" />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id alias ex
+        cupiditate aspernatur odio facilis accusantium ea amet, adipisci
+        expedita omnis soluta repudiandae. Maxime incidunt odio, reprehenderit
+        recusandae repudiandae autem!
+      </p>
+    </figure>
+  );
+}
+
+export default Card;
